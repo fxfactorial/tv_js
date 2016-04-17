0d436d93aea0019d2a5f79597643b497a49eaf39
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-04-17 09:40:16 -0700

    Working example

--- example/example_app/tv_code.ml
+++ example/example_app/tv_code.ml
@@ -2,6 +2,19 @@ open Tv_js
 
 module P = Printf
 
+let launch_video () =
+  (* var mediaItem = new MediaItem('video', 'http://trailers.apple.com/movies/\ *)
+  (* focus_features/9/9-clip_480p.mov');\ *)
+  Js.Unsafe.eval_string
+    "(function (){\
+     var player = new Player();\
+     var playlist = new Playlist();\
+     var mediaItem = new MediaItem('video', \
+     'http://localhost:9001/working_server.mov');\
+     player.playlist = playlist;\
+     player.playlist.push(mediaItem);\
+     player.present();})()"
+
 module Delegate = Make(struct
 
     let on_exit obj =
@@ -20,12 +33,13 @@ module Delegate = Make(struct
     (* Your entry point to the application, think
        ApplicationDidFinishLaunching *)
     let on_launch obj =
-      (match obj#launch_context, obj#location with
-       | (Some l_ctx, Some loc) ->
-         P.sprintf "Launch context: %s, Location: %s" l_ctx loc
-       | (None, Some loc) -> P.sprintf "Only: Location: %s" loc
-       | (Some l_ctx, None) -> P.sprintf "Only: launch context: %s" l_ctx
-       | _ -> P.sprintf "Neither launch context or location provided from system")
-      |> print_endline
+      ((match obj#launch_context, obj#location with
+          | (Some l_ctx, Some loc) ->
+            P.sprintf "Launch context: %s, Location: %s" l_ctx loc
+          | (None, Some loc) -> P.sprintf "Only: Location: %s" loc
+          | (Some l_ctx, None) -> P.sprintf "Only: launch context: %s" l_ctx
+          | _ -> P.sprintf "Neither launch context or location provided from system")
+       |> print_endline);
+      launch_video ()
 
   end)
